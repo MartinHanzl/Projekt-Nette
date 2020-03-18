88b1ec5af0c3c992414f813d146b657529afd3d2
MartinHanzl <martas.hanzl@email.cz>
2020-03-18 10:16:13 +0100

    First commit

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -5,7 +5,8 @@ declare(strict_types=1);
 namespace App\Presenters;
 
 use Nette;
-
+use Nette\Application\UI;
+use Nette\Application\UI\Form;
 
 final class HomepagePresenter extends Nette\Application\UI\Presenter
 {
@@ -27,5 +28,23 @@ final class HomepagePresenter extends Nette\Application\UI\Presenter
 
     public function renderPrispevek($id) :void {
         $this->template->prispevky = $this->database->table('prispevky')->get($id);
+    }
+
+    public function renderAkce() :void {
+        $this->template->akce = $this->database->table('akce')
+            ->order('akceID DESC');
+    }
+
+    protected function createComponentContactForm() :Form {
+        $form = new Form;
+        $form->addEmail('email')
+                ->setHtmlAttribute('placeholder', 'Vaše emailová adresa')
+                ->setRequired("Zadejte platnou emailovou adresu!");
+        $form->addText('predmet')
+                ->setHtmlAttribute('placeholder', 'Předmět emailu')
+                ->setRequired("Zadejte předmět emailu!");
+        $form->addSubmit('login', 'Registrovat');
+        $form->onSuccess[] = [$this, 'registrationFormSucceeded'];
+        return $form;
     }
 }
